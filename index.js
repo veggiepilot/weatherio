ceaa7e2cd4ba8e2effd28e0e335d0e0d75e10f68
Adam <adamolsey>
2021-12-12 18:49:03 -0500

    ui fixes

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// Search feild DOM element, element value, DOM button element //
 const userSearchEL = document.querySelector("#user-form");
 const citySearchInput = document.querySelector("#city");
 const submitBtn = document.querySelector(".button");
-const localBtn = document.querySelector(".data-button")
-// DOM elements to display
-// City, Tempature, Wind, Humidity, UV Index,  //
+const localBtn = document.querySelector(".data-button");
+
 const citySearchDisplay = document.querySelector(".city");
 const cityTempDisplay = document.querySelector("#temp");
 const cityWindDispay = document.querySelector("#wind");
 const cityHumidityDisplay = document.querySelector("#humidity");
 const cityUVindexDisplay = document.querySelector("#uvi");
 
-// Listen for user submision and then fetch the data //
+const fiveDayTempDisplay = document.querySelector("#t");
+const fiveDayWindDispay = document.querySelector("#w");
+const fiveDayHumidityDisplay = document.querySelector("#h");
+const fiveDayDateDisplay = document.querySelector("#date");
+const fiveDayImageDispay = document.querySelector("#img");
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&appid=9c66b2e77274737adc119b1bd2f997a7&units=Imperial`
+    `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value},840&appid=9c66b2e77274737adc119b1bd2f997a7&units=Imperial`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -23,73 +26,50 @@ submitBtn.addEventListener("click", (e) => {
       let tempValue = data["main"]["temp"];
       let windValue = data["wind"]["speed"];
       let humidityValue = data["main"]["humidity"];
-      let lat =  data["coord"]["lat"]
-      let lon = data["coord"]["lon"]
+      let lat = data["coord"]["lat"];
+      let lon = data["coord"]["lon"];
       citySearchDisplay.innerText = cityValue;
       cityTempDisplay.innerText = tempValue;
       cityWindDispay.innerText = windValue;
       cityHumidityDisplay.innerText = humidityValue;
-      localStorage.setItem("cityvalue",cityValue)
+      localStorage.setItem("cityvalue", cityValue);
       var cities = [];
-      let store = (cityValue) => {
-      cities.push(store);
-      localStorage.setItem("cities",JSON.stringify(cities))
-  }
-      localBtn.innerText = store[cities]
-
-  fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=` +
-      lat +`&lon=` + lon + 
-        `&appid=9c66b2e77274737adc119b1bd2f997a7&units=imperial`
+      let store = () => {
+        cities.push(store);
+        localStorage.setItem("cities", JSON.stringify(cities));
+      };
+      localBtn.innerText = cityValue;
+      console.log(data);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=` +
+          lat +
+          `&lon=` +
+          lon +
+          `&appid=9c66b2e77274737adc119b1bd2f997a7&units=imperial`
       )
-      .then((response) => response.json())
-      .then((data) => {
-        let uviValue = data["current"]["uvi"];
-        let fiveDay = data["daily"]
-        let days = []
-        days.push(fiveDay) 
-        for (let days =0; days <= 6; days++) {
-          days += fiveDay[days]
-        }
-        cityUVindexDisplay.innerText = uviValue;
-        
-      })
-    })  .catch((err) => console.log(err));
+        .then((response) => response.json())
+        .then((data) => {
+          let uviValue = data["current"]["uvi"];
+          cityUVindexDisplay.innerText = uviValue;
+        });
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${citySearchInput.value}&appid=9c66b2e77274737adc119b1bd2f997a7&units=Imperial`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          let date = data["list"][0]["dt_txt"];
+          let temp = data["list"][0]["main"]["temp"];
+          let wind = data["list"][0]["wind"]["speed"];
+          let humid = data["list"][0]["main"]["humidity"];
+          let icon = data["list"][0]["weather"]["icon"];
+          fiveDayTempDisplay.innerText = temp;
+          fiveDayWindDispay.innerText = wind;
+          fiveDayHumidityDisplay.innerText = humid;
+          fiveDayDateDisplay.innerText = date;
+          fiveDayImageDispay.innerText = icon;
+        });
+    })
+
+    .catch((err) => console.log(err));
 });
-
-
-
-
-
-
-
-// // Listen for user submision and then fetch the data //
-// localBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/weather?q=${citySearchInput.value}&appid=9c66b2e77274737adc119b1bd2f997a7&units=Imperial`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       let cityValue = citySearchInput.value;
-//       let tempValue = data["main"]["temp"];
-//       let windValue = data["wind"]["speed"];
-//       let humidityValue = data["main"]["humidity"];
-//       let lat =  data["coord"]["lat"]
-//       let lon = data["coord"]["lon"]
-//       citySearchDisplay.innerText = cityValue;
-//       cityTempDisplay.innerText = tempValue;
-//       cityWindDispay.innerText = windValue;
-//       cityHumidityDisplay.innerText = humidityValue;
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/onecall?lat=` +
-//       lat +`&lon=` + lon + 
-//         `&appid=9c66b2e77274737adc119b1bd2f997a7&units=imperial`
-//       )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         let uviValue = data["current"]["uvi"];
-//         cityUVindexDisplay.innerText = uviValue;
-//       })
-//     })  .catch((err) => console.log(err));
-// });
